206cf1acd7b35d6f02a50be9f7ff80600af45bf7
Alexhandru <alextitieni@yahoo.com>
2025-06-23 11:54:23 +0300

    Remove unused BalanceDelta

--- src/general/VolumeBasedFeeHook.sol
+++ src/general/VolumeBasedFeeHook.sol
@@ -6,7 +6,6 @@ import {Hooks} from "v4-core/src/libraries/Hooks.sol";
 import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
 import {PoolKey} from "v4-core/src/types/PoolKey.sol";
 import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
-import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
 import {CustomRevert} from "v4-core/src/libraries/CustomRevert.sol";
 import {SwapParams} from "v4-core/src/types/PoolOperation.sol";
