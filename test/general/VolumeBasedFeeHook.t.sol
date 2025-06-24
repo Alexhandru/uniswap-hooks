da2ade9bfb1faba72d94a1ff5e56248ec2a91323
Alexhandru <alextitieni@yahoo.com>
2025-06-24 17:41:58 +0300

    Change upper fuzz test threshold + format test file

--- test/general/VolumeBasedFeeHook.t.sol
+++ test/general/VolumeBasedFeeHook.t.sol
@@ -15,6 +15,7 @@ import {IPositionManager} from "v4-periphery/src/interfaces/IPositionManager.sol
 import {VolumeBasedFeeHook} from "../../src/general/VolumeBasedFeeHook.sol";
 import {SwapParams} from "v4-core/src/types/PoolOperation.sol";
 import {Deployers} from "v4-core/test/utils/Deployers.sol";
+import {SafeCast} from "v4-core/src/libraries/SafeCast.sol";
 
 contract VolumeBasedFeeHookTest is Test, Deployers {
     using StateLibrary for IPoolManager;
@@ -33,11 +34,9 @@ contract VolumeBasedFeeHookTest is Test, Deployers {
     uint256 minAmount1 = 1e18;
     uint256 maxAmount1 = 10e18;
 
-    uint160 flags = uint160(
-                Hooks.BEFORE_SWAP_FLAG 
-            ) ^ (0x4444 << 144); // Namespace the hook to avoid collisions
+    uint160 flags = uint160(Hooks.BEFORE_SWAP_FLAG) ^ (0x4444 << 144); // Namespace the hook to avoid collisions
 
-    VolumeBasedFeeHook.SwapVolumeParams swapVolumeParams = 
+    VolumeBasedFeeHook.SwapVolumeParams swapVolumeParams =
         VolumeBasedFeeHook.SwapVolumeParams({
             defaultFee: defaultFee,
             feeAtMinAmount0: feeAtMinAmount0,
@@ -56,28 +55,25 @@ contract VolumeBasedFeeHookTest is Test, Deployers {
     function setUp() public {
         deployFreshManagerAndRouters();
 
-        hook = VolumeBasedFeeHook(
-            address(flags)
-        );
+        hook = VolumeBasedFeeHook(address(flags));
 
-        newHook = VolumeBasedFeeHook(address(
-            uint160(
-                Hooks.BEFORE_SWAP_FLAG 
-            ) + 2 ** 96
-        ));
+        newHook = VolumeBasedFeeHook(
+            address(uint160(Hooks.BEFORE_SWAP_FLAG) + 2 ** 96)
+        );
 
         deployCodeTo(
             "VolumeBasedFeeHook.sol:VolumeBasedFeeHook",
-            abi.encode(
-                manager,
-                swapVolumeParams
-            ),
+            abi.encode(manager, swapVolumeParams),
             address(hook)
         );
 
         deployMintAndApprove2Currencies();
-        (key,) = initPoolAndAddLiquidity(
-            currency0, currency1, IHooks(address(hook)), LPFeeLibrary.DYNAMIC_FEE_FLAG, SQRT_PRICE_1_1
+        (key, ) = initPoolAndAddLiquidity(
+            currency0,
+            currency1,
+            IHooks(address(hook)),
+            LPFeeLibrary.DYNAMIC_FEE_FLAG,
+            SQRT_PRICE_1_1
         );
     }
 
@@ -86,10 +82,7 @@ contract VolumeBasedFeeHookTest is Test, Deployers {
 
         swapVolumeParams.feeAtMinAmount0 = invalidFeeAtMinAmount0;
 
-        bytes memory constructorArgs = abi.encode(
-            manager,
-            swapVolumeParams
-        );
+        bytes memory constructorArgs = abi.encode(manager, swapVolumeParams);
 
         vm.expectRevert(VolumeBasedFeeHook.InvalidFees.selector);
         deployCodeTo(
@@ -105,7 +98,7 @@ contract VolumeBasedFeeHookTest is Test, Deployers {
         swapVolumeParams.feeAtMaxAmount0 = invalidFeeAtMaxAmount0;
 
         bytes memory constructorArgs = abi.encode(manager, swapVolumeParams);
-  
+
         vm.expectRevert(VolumeBasedFeeHook.InvalidFees.selector);
         deployCodeTo(
             "VolumeBasedFeeHook.sol:VolumeBasedFeeHook",
@@ -187,7 +180,7 @@ contract VolumeBasedFeeHookTest is Test, Deployers {
         bool zeroForOne = true;
         int256 amountSpecified = -1e18; // negative number indicates exact input swap!
         swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
-        
+
         // Check the swap amounts
         assertEq(_fetchPoolLPFee(key), feeAtMinAmount0);
     }
@@ -222,7 +215,12 @@ contract VolumeBasedFeeHookTest is Test, Deployers {
     function test_swap_middleVolume_exactInput_zeroForOne() public {
         bool zeroForOne = true;
         int256 amountSpecified = -int256((minAmount0 + maxAmount0) / 2); // Dynamically calculate middle value
-        uint24 expectedFee = uint24(feeAtMinAmount0 - ((feeAtMinAmount0 - feeAtMaxAmount0) * ((minAmount0 + maxAmount0) / 2 - minAmount0)) / (maxAmount0 - minAmount0));
+        uint24 expectedFee = uint24(
+            feeAtMinAmount0 -
+                ((feeAtMinAmount0 - feeAtMaxAmount0) *
+                    ((minAmount0 + maxAmount0) / 2 - minAmount0)) /
+                (maxAmount0 - minAmount0)
+        );
         swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
 
         // Check the swap amounts
@@ -232,7 +230,12 @@ contract VolumeBasedFeeHookTest is Test, Deployers {
     function test_swap_middleVolume_exactOutput_zeroForOne() public {
         bool zeroForOne = true;
         int256 amountSpecified = int256((minAmount1 + maxAmount1) / 2); // Dynamically calculate middle value
-        uint24 expectedFee = uint24(feeAtMinAmount1 - ((feeAtMinAmount1 - feeAtMaxAmount1) * ((minAmount1 + maxAmount1) / 2 - minAmount1)) / (maxAmount1 - minAmount1));
+        uint24 expectedFee = uint24(
+            feeAtMinAmount1 -
+                ((feeAtMinAmount1 - feeAtMaxAmount1) *
+                    ((minAmount1 + maxAmount1) / 2 - minAmount1)) /
+                (maxAmount1 - minAmount1)
+        );
         swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
 
         // Check the swap amounts
@@ -242,7 +245,12 @@ contract VolumeBasedFeeHookTest is Test, Deployers {
     function test_swap_middleVolume_exactInput_notZeroForOne() public {
         bool zeroForOne = false;
         int256 amountSpecified = -int256((minAmount1 + maxAmount1) / 2); // Dynamically calculate middle value
-        uint24 expectedFee = uint24(feeAtMinAmount1 - ((feeAtMinAmount1 - feeAtMaxAmount1) * ((minAmount1 + maxAmount1) / 2 - minAmount1)) / (maxAmount1 - minAmount1));
+        uint24 expectedFee = uint24(
+            feeAtMinAmount1 -
+                ((feeAtMinAmount1 - feeAtMaxAmount1) *
+                    ((minAmount1 + maxAmount1) / 2 - minAmount1)) /
+                (maxAmount1 - minAmount1)
+        );
         swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
 
         // Check the swap amounts
@@ -252,7 +260,12 @@ contract VolumeBasedFeeHookTest is Test, Deployers {
     function test_swap_middleVolume_exactOutput_notZeroForOne() public {
         bool zeroForOne = false;
         int256 amountSpecified = int256((minAmount0 + maxAmount0) / 2); // Dynamically calculate middle value
-        uint24 expectedFee = uint24(feeAtMinAmount0 - ((feeAtMinAmount0 - feeAtMaxAmount0) * ((minAmount0 + maxAmount0) / 2 - minAmount0)) / (maxAmount0 - minAmount0));
+        uint24 expectedFee = uint24(
+            feeAtMinAmount0 -
+                ((feeAtMinAmount0 - feeAtMaxAmount0) *
+                    ((minAmount0 + maxAmount0) / 2 - minAmount0)) /
+                (maxAmount0 - minAmount0)
+        );
         swap(key, zeroForOne, amountSpecified, ZERO_BYTES);
 
         // Check the swap amounts
@@ -261,17 +274,18 @@ contract VolumeBasedFeeHookTest is Test, Deployers {
 
     function test_swap_sameMinMaxFees() public {
         // Set up new params where min and max fees are the same
-        VolumeBasedFeeHook.SwapVolumeParams memory sameFeesParams = VolumeBasedFeeHook.SwapVolumeParams({
-            defaultFee: 3000,
-            feeAtMinAmount0: 2000, // Same fee for min and max amount0
-            feeAtMaxAmount0: 2000,
-            feeAtMinAmount1: 1500, // Same fee for min and max amount1
-            feeAtMaxAmount1: 1500,
-            minAmount0: 1e18,
-            maxAmount0: 10e18,
-            minAmount1: 1e18,
-            maxAmount1: 10e18
-        });
+        VolumeBasedFeeHook.SwapVolumeParams
+            memory sameFeesParams = VolumeBasedFeeHook.SwapVolumeParams({
+                defaultFee: 3000,
+                feeAtMinAmount0: 2000, // Same fee for min and max amount0
+                feeAtMaxAmount0: 2000,
+                feeAtMinAmount1: 1500, // Same fee for min and max amount1
+                feeAtMaxAmount1: 1500,
+                minAmount0: 1e18,
+                maxAmount0: 10e18,
+                minAmount1: 1e18,
+                maxAmount1: 10e18
+            });
 
         deployCodeTo(
             "VolumeBasedFeeHook.sol:VolumeBasedFeeHook",
@@ -280,11 +294,11 @@ contract VolumeBasedFeeHookTest is Test, Deployers {
         );
 
         // Initialize new pool with the hook
-        (PoolKey memory newKey,) = initPoolAndAddLiquidity(
-            currency0, 
-            currency1, 
-            IHooks(address(newHook)), 
-            LPFeeLibrary.DYNAMIC_FEE_FLAG, 
+        (PoolKey memory newKey, ) = initPoolAndAddLiquidity(
+            currency0,
+            currency1,
+            IHooks(address(newHook)),
+            LPFeeLibrary.DYNAMIC_FEE_FLAG,
             SQRT_PRICE_1_1
         );
 
@@ -292,13 +306,21 @@ contract VolumeBasedFeeHookTest is Test, Deployers {
         bool zeroForOne = true;
         int256 amountSpecified = -5e18; // Amount between min and max
         swap(newKey, zeroForOne, amountSpecified, ZERO_BYTES);
-        assertEq(_fetchPoolLPFee(newKey), 2000, "Fee should be 2000 for token0 swap");
+        assertEq(
+            _fetchPoolLPFee(newKey),
+            2000,
+            "Fee should be 2000 for token0 swap"
+        );
 
         // Test swap for token1
         zeroForOne = false;
         amountSpecified = -5e18; // Amount between min and max
         swap(newKey, zeroForOne, amountSpecified, ZERO_BYTES);
-        assertEq(_fetchPoolLPFee(newKey), 1500, "Fee should be 1500 for token1 swap");
+        assertEq(
+            _fetchPoolLPFee(newKey),
+            1500,
+            "Fee should be 1500 for token1 swap"
+        );
     }
 
     function test_fuzz_swapParams(
@@ -329,24 +351,22 @@ contract VolumeBasedFeeHookTest is Test, Deployers {
         );
         _setupAmountSpecified(_amountSpecified);
 
-        VolumeBasedFeeHook.SwapVolumeParams memory params = VolumeBasedFeeHook.SwapVolumeParams({
-            defaultFee: defaultFee,
-            feeAtMinAmount0: feeAtMinAmount0,
-            feeAtMaxAmount0: feeAtMaxAmount0,
-            feeAtMinAmount1: feeAtMinAmount1,
-            feeAtMaxAmount1: feeAtMaxAmount1,
-            minAmount0: minAmount0,
-            maxAmount0: maxAmount0,
-            minAmount1: minAmount1,
-            maxAmount1: maxAmount1
-        });
+        VolumeBasedFeeHook.SwapVolumeParams memory params = VolumeBasedFeeHook
+            .SwapVolumeParams({
+                defaultFee: defaultFee,
+                feeAtMinAmount0: feeAtMinAmount0,
+                feeAtMaxAmount0: feeAtMaxAmount0,
+                feeAtMinAmount1: feeAtMinAmount1,
+                feeAtMaxAmount1: feeAtMaxAmount1,
+                minAmount0: minAmount0,
+                maxAmount0: maxAmount0,
+                minAmount1: minAmount1,
+                maxAmount1: maxAmount1
+            });
 
-
-        VolumeBasedFeeHookHarness swapVolume = VolumeBasedFeeHookHarness(address(
-            uint160(
-                Hooks.BEFORE_SWAP_FLAG 
-            ) + 2 ** 96
-        ));
+        VolumeBasedFeeHookHarness swapVolume = VolumeBasedFeeHookHarness(
+            address(uint160(Hooks.BEFORE_SWAP_FLAG) + 2 ** 96)
+        );
 
         deployCodeTo(
             "VolumeBasedFeeHook.t.sol:VolumeBasedFeeHookHarness",
@@ -354,8 +374,12 @@ contract VolumeBasedFeeHookTest is Test, Deployers {
             address(swapVolume)
         );
 
-        (key,) = initPoolAndAddLiquidity(
-            currency0, currency1, IHooks(address(swapVolume)), LPFeeLibrary.DYNAMIC_FEE_FLAG, SQRT_PRICE_1_1
+        (key, ) = initPoolAndAddLiquidity(
+            currency0,
+            currency1,
+            IHooks(address(swapVolume)),
+            LPFeeLibrary.DYNAMIC_FEE_FLAG,
+            SQRT_PRICE_1_1
         );
 
         _validateParams(swapVolume.getSwapVolumeParams());
@@ -366,11 +390,17 @@ contract VolumeBasedFeeHookTest is Test, Deployers {
             SwapParams({
                 zeroForOne: zeroForOneFuzz == 1,
                 amountSpecified: amountSpecifiedFuzz,
-                sqrtPriceLimitX96: zeroForOneFuzz == 1 ? MIN_PRICE_LIMIT : MAX_PRICE_LIMIT
+                sqrtPriceLimitX96: zeroForOneFuzz == 1
+                    ? MIN_PRICE_LIMIT
+                    : MAX_PRICE_LIMIT
             })
         );
 
-        assertEq(_fetchPoolLPFee(key), expectedFee, "LP fee mismatch after swap");
+        assertEq(
+            _fetchPoolLPFee(key),
+            expectedFee,
+            "LP fee mismatch after swap"
+        );
     }
 
     function _setupFuzzedParams(
@@ -391,73 +421,121 @@ contract VolumeBasedFeeHookTest is Test, Deployers {
         feeAtMaxAmount0 = uint24(bound(_feeAtMaxAmount0, 0, feeAtMinAmount0));
         feeAtMinAmount1 = uint24(bound(_feeAtMinAmount1, 0, defaultFee));
         feeAtMaxAmount1 = uint24(bound(_feeAtMaxAmount1, 0, feeAtMinAmount1));
-        minAmount0 = bound(_minAmount0, 1, 10e18 - 1);
-        maxAmount0 = bound(_maxAmount0, minAmount0 + 1, 10e18);
-        minAmount1 = bound(_minAmount1, 1, 10e18 - 1);
-        maxAmount1 = bound(_maxAmount1, minAmount1 + 1, 10e18);
+        minAmount0 = bound(_minAmount0, 1, uint256(type(uint128).max) - 1);
+        maxAmount0 = bound(
+            _maxAmount0,
+            minAmount0 + 1,
+            uint256(type(uint128).max)
+        );
+        minAmount1 = bound(_minAmount1, 1, uint256(type(uint128).max) - 1);
+        maxAmount1 = bound(
+            _maxAmount1,
+            minAmount1 + 1,
+            uint256(type(uint128).max)
+        );
         zeroForOneFuzz = _zeroForOne % 2;
         amountSpecifiedFuzz = _amountSpecified;
     }
 
     function _setupAmountSpecified(int256 _amountSpecified) private {
-        vm.assume(amountSpecifiedFuzz > 0);
+        vm.assume(
+            _amountSpecified != 0 && _amountSpecified != type(int256).min
+        );
 
-        bool exactInput = amountSpecifiedFuzz < 0 ? true : false; // Check if it's an exact input swap
-        int256 minAmount = zeroForOneFuzz == 1 ? 
-            (exactInput ? int256(minAmount0) : int256(minAmount1)) : 
-            (exactInput ? int256(minAmount1) : int256(minAmount0));
-        int256 maxAmount = zeroForOneFuzz == 1 ? 
-            (exactInput ? int256(maxAmount0) : int256(maxAmount1)) : 
-            (exactInput ? int256(maxAmount1) : int256(maxAmount0));
+        bool exactInput = _amountSpecified < 0;
+        int256 absAmountSpecified = exactInput
+            ? -_amountSpecified
+            : _amountSpecified;
 
-        if (amountSpecifiedFuzz % 3 == 0) {
-            amountSpecifiedFuzz = bound(
-                amountSpecifiedFuzz,
-                int256(1),
-                minAmount == 1 ? int256(1) : minAmount - 1 
+        uint256 minAmount = zeroForOneFuzz == 1
+            ? (exactInput ? minAmount0 : minAmount1)
+            : (exactInput ? minAmount1 : minAmount0);
+
+        uint256 maxAmount = zeroForOneFuzz == 1
+            ? (exactInput ? maxAmount0 : maxAmount1)
+            : (exactInput ? maxAmount1 : maxAmount0);
+
+        uint256 boundedAmount;
+        // The goal is to test swaps with amounts that are lower than, between, and higher than the min/max thresholds.
+        // We use modulo to distribute the fuzzed amounts across these three scenarios.
+        uint256 scenario = uint256(absAmountSpecified) % 3;
+
+        if (scenario == 0) {
+            // Scenario 1: Swap amount is less than the minimum threshold. Fee should be defaultFee.
+            boundedAmount = bound(
+                uint256(absAmountSpecified),
+                1,
+                minAmount > 1 ? minAmount - 1 : 1
             );
-        } else if (amountSpecifiedFuzz % 3 == 1) {
-            amountSpecifiedFuzz = bound(
-                amountSpecifiedFuzz,
+        } else if (scenario == 1) {
+            // Scenario 2: Swap amount is between the minimum and maximum thresholds. Fee should be interpolated.
+            boundedAmount = bound(
+                uint256(absAmountSpecified),
                 minAmount,
                 maxAmount
             );
-        } else if (amountSpecifiedFuzz % 3 == 2) {
-            amountSpecifiedFuzz = bound(
-                amountSpecifiedFuzz,
-                maxAmount + 1, 
-                int256(1e40)
+        } else {
+            // scenario == 2
+            // Scenario 3: Swap amount is greater than the maximum threshold. Fee should be feeAtMaxAmount.
+            boundedAmount = bound(
+                uint256(absAmountSpecified),
+                maxAmount,
+                uint256(type(uint128).max)
             );
         }
 
-        amountSpecifiedFuzz = _amountSpecified < 0 ? -amountSpecifiedFuzz : amountSpecifiedFuzz;
+        amountSpecifiedFuzz = exactInput
+            ? -int256(boundedAmount)
+            : int256(boundedAmount);
     }
 
-    function _validateParams(VolumeBasedFeeHook.SwapVolumeParams memory paramsOut) view private {
+    function _validateParams(
+        VolumeBasedFeeHook.SwapVolumeParams memory paramsOut
+    ) private view {
         assertEq(paramsOut.defaultFee, defaultFee, "defaultFee mismatch");
-        assertEq(paramsOut.feeAtMinAmount0, feeAtMinAmount0, "feeAtMinAmount0 mismatch");
-        assertEq(paramsOut.feeAtMaxAmount0, feeAtMaxAmount0, "feeAtMaxAmount0 mismatch");
-        assertEq(paramsOut.feeAtMinAmount1, feeAtMinAmount1, "feeAtMinAmount1 mismatch");
-        assertEq(paramsOut.feeAtMaxAmount1, feeAtMaxAmount1, "feeAtMaxAmount1 mismatch");
+        assertEq(
+            paramsOut.feeAtMinAmount0,
+            feeAtMinAmount0,
+            "feeAtMinAmount0 mismatch"
+        );
+        assertEq(
+            paramsOut.feeAtMaxAmount0,
+            feeAtMaxAmount0,
+            "feeAtMaxAmount0 mismatch"
+        );
+        assertEq(
+            paramsOut.feeAtMinAmount1,
+            feeAtMinAmount1,
+            "feeAtMinAmount1 mismatch"
+        );
+        assertEq(
+            paramsOut.feeAtMaxAmount1,
+            feeAtMaxAmount1,
+            "feeAtMaxAmount1 mismatch"
+        );
         assertEq(paramsOut.minAmount0, minAmount0, "minAmount0 mismatch");
         assertEq(paramsOut.maxAmount0, maxAmount0, "maxAmount0 mismatch");
         assertEq(paramsOut.minAmount1, minAmount1, "minAmount1 mismatch");
         assertEq(paramsOut.maxAmount1, maxAmount1, "maxAmount1 mismatch");
     }
 
-    function _fetchPoolLPFee(PoolKey memory _key) internal view returns (uint256 lpFee) {
+    function _fetchPoolLPFee(
+        PoolKey memory _key
+    ) internal view returns (uint256 lpFee) {
         PoolId id = _key.toId();
-        (,,, lpFee) = manager.getSlot0(id);
+        (, , , lpFee) = manager.getSlot0(id);
     }
 }
 
 contract VolumeBasedFeeHookHarness is VolumeBasedFeeHook {
-
-    constructor(IPoolManager _poolManager, SwapVolumeParams memory params) VolumeBasedFeeHook(_poolManager, params) {}
+    constructor(
+        IPoolManager _poolManager,
+        SwapVolumeParams memory params
+    ) VolumeBasedFeeHook(_poolManager, params) {}
 
     function exposed_calculateFee(
         SwapParams calldata swapParams
     ) external view returns (uint24) {
         return _calculateFee(swapParams);
     }
-}   
+}
